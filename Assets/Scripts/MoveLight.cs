fa3863a17d42bb130ffa26e2a49ac91df64a1224
Austin Beeler <abeeler1@uncc.edu>
2016-11-01 22:12:05 -0400

    Fixes player mouse tracking to use new coordinate system

--- Assets/Scripts/MoveLight.cs
+++ Assets/Scripts/MoveLight.cs
@@ -24,16 +24,15 @@ public class MoveLight : MonoBehaviour {
         //if any of the movement buttons(toDO: controller support) are being moved
         if (moveHorizontal != 0 || moveVertical != 0){
             //Determine which movement type to use.  (toDO: change runSpeed to call altMove on character class)
-            rb.velocity = (Input.GetKey(KeyCode.Space)) ? movement * runSpeed : movement * normalSpeed;
+            rb.velocity = movement * (Input.GetKey(KeyCode.Space) ?  runSpeed : normalSpeed);
         }
 
         //This rotates the player to face the mouse(toDO: controller support)
-        Vector2 mousePosition = Input.mousePosition;
-        var distance = (transform.position.y - Camera.main.transform.position.y);
-        var screenPosition = new Vector3(mousePosition.x, mousePosition.y, distance);
-        screenPosition = Camera.main.ScreenToWorldPoint(screenPosition);
-        Debug.Log("ScreenPos "+ screenPosition);
-        transform.eulerAngles = new Vector3(defaultRotationX, 0.0f, Mathf.Atan2((screenPosition.y - transform.position.y), (screenPosition.x - transform.position.x)) * Mathf.Rad2Deg - 90);
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = mousePosition.y;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        //Debug.Log(Input.mousePosition + " -> " + mousePosition);
+        transform.eulerAngles = new Vector3(defaultRotationX, 0, Mathf.Atan2((mousePosition.z - transform.position.z), (mousePosition.x - transform.position.x)) * Mathf.Rad2Deg - 90);
         //Debug.Log(transform.eulerAngles);
 
     }
